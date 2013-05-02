0be349151857b5ac6eb88744ec827e50a64bb477
Ye Lin Kyaw <yelin@amdon.com>
2013-05-02 21:06:02 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-aws-s3-backup
-=============
+AWS S3 Backup
+===================
+Simple python script to backup the list of local files to S3.
+It can be executed from crontab for automated snapshot.
+
+Requirements
+===================
+1. Python 2.7 and later
+2. Boto 2.9 and later
+
+Configurations
+===================
+1. Update aws_access_key and aws_secret_key
+2. Update bucket_name to store backup file
+3. Update bucket_location
+4. Update backup.conf
---
+++ backup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import tarfile
+import datetime
+import tempfile
+import os
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+# AWS Constants
+aws_access_key = 'AWS Access Key'
+aws_secret_key = 'AWS Secret Key'
+bucket_name = 'Bucket Name'
+bucket_location = 'backup'
+
+# Current Datetime 
+now = datetime.datetime.now()
+
+# Temp Archvie
+temp_directory = tempfile.gettempdir()
+
+# Get List to Archive
+lines = [line.strip() for line in open('backup.conf')]
+
+# Starting Backup
+print 'Starting Backup'
+for line in lines:
+	if os.path.isfile(line) or os.path.isdir(line):
+		# Archive
+		source_full_path = line
+		source_directory_name = os.path.basename(source_full_path)
+		print 'Start Archiving ' + source_full_path
+		archive_file_name = source_directory_name + '-repos-backup-' + now.strftime('%d-%m-%Y') + '.tar.gz'
+		archive_full_path = os.path.join(temp_directory, archive_file_name)
+		print 'Archiving finish'
+		# Create Archive
+		tar = tarfile.open( archive_full_path , 'w:gz')
+		tar.add(source_full_path, arcname=source_directory_name)
+		tar.close()
+		# Accessing S3
+		con = S3Connection(aws_access_key, aws_secret_key)
+		bucket = con.get_bucket(bucket_name)
+		object = Key(bucket)
+		object.key = os.path.join(bucket_location, archive_file_name)
+		print 'Uploading ' + archive_file_name + ' to ' + bucket_name + '/' + bucket_location
+		object.set_contents_from_filename(archive_full_path)
+		print 'Uploading finish'
+		# Clear Temp File
+		os.remove(archive_full_path)
+	else:
+		# Invalid Path
+		print 'Invalid archive path'
+print 'Finish Backup'
